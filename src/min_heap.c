4b7d7bec13e61a5920f9792dcf446ff71c84350f
cblberlin <bailin.cai@hotmail.fr>
2021-12-04 05:23:15 +0100

    min heap modified

--- src/min_heap.c
+++ src/min_heap.c
@@ -14,15 +14,26 @@ void swap(node* n1, node* n2){
     we start the index by 0;
 */
 void init_heap(min_heap* heap, int n){
-    heap = (min_heap *) malloc(sizeof(min_heap));
+    //heap = (min_heap*)malloc(sizeof(min_heap));
     heap->size = n;
     if(heap == NULL){
         printf("Memory Error!\n");
         return;
     }
-    heap->nodes = malloc(sizeof(node) * n);
+    heap->nodes = (node*)malloc(sizeof(node) * n);
+    for(int i = 0; i < n; i++){
+        heap->nodes[i].prev = 0;
+    }
     heap->size = n;
     heap->tail = 0; 
+}
+/*
+    Initialisation of a node
+*/
+void init_node(node* n){
+    n->index = 0;
+    n->weight = 0.;
+    n->prev = 0;
 }
 
 void free_heap(min_heap* heap){
@@ -41,17 +52,65 @@ bool heap_empty(const min_heap* heap){
 }
 
 /*
-    insert a val to the heap and the heap maintain min-heap properties
+    The following algorithm can be used to “fix” an array not necessarily
+    satisfying the heap property.
 */
-void heap_push(min_heap* heap, double val){
-
+void Heapify(min_heap* heap, int i){
+    int smallest = (CHILD_LEFT(i) < heap->size && heap->nodes[CHILD_LEFT(i)].weight < heap->nodes[i].weight) ? CHILD_LEFT(i) : i ;
+    if(CHILD_RIGHT(i) < heap->size && heap->nodes[CHILD_RIGHT(i)].weight < heap->nodes[smallest].weight) {
+        smallest = CHILD_RIGHT(i) ;
+    }
+    if(smallest != i) {
+        swap(&(heap->nodes[i]), &(heap->nodes[smallest])) ;
+        Heapify(heap, smallest) ;
+    }
 }
 
-/*double extractMin(min_heap* heap){
-    double min = INFINITY;
-
-    for(int i = 0; i < heap->size; i++){
-        if( heap->data[i] )
+/*
+    Decrease the key of current node and maintain the property of min heap
+*/
+void DecreaseKey(min_heap* heap, node* x, double val){
+    if (val > heap->nodes[x->index].weight){
+        return;
     }
-}*/
+    heap->nodes[x->index].weight = val;
+    while(x->index > 0 && heap->nodes[PARENT(x->index)].weight > heap->nodes[x->index].weight){
+        swap(&heap->nodes[x->index], &heap->nodes[PARENT(x->index)]);
+        heap->nodes[x->index] = heap->nodes[PARENT(x->index)];
+    }
+}
 
+/*
+    insert a val to the heap and the heap maintain min-heap properties
+*/
+void Insert(min_heap* heap, node* x){
+    heap->tail++;
+    x->index = heap->tail;
+    heap->nodes[1] = *x;
+    DecreaseKey(heap, x, x->weight);
+}
+
+/*
+    delete the node with minimum key in the heap and maintain the min heap property
+*/
+node ExtractMin(min_heap* heap){
+    if(heap->tail < 0){
+        fprintf(stderr, "Heap underflow");
+    }
+    node min = heap->nodes[0];
+    heap->nodes[0] = heap->nodes[heap->tail];
+    heap->tail--;
+    Heapify(heap, 0);
+    return min;
+}
+
+/*
+    update the weight of v to w + u->weight 
+    in the heap and maintain the min heap property
+*/
+void Relax(min_heap* heap, node* u, node* v, double w){
+    if(v->weight > u->weight + w){
+        DecreaseKey(heap, v, u->weight + w);
+        v->prev = u->index;
+    }
+}
